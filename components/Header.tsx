3af634268e3178ab905182a86a7df41759976959
aldrn <aldrn29@gmail.com>
2022-07-16 23:54:49 +0900

    feat : Header 파일 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
+import Add from "../public/images/add.png";
+import Heart from "../public/images/heart.png";
+import Send from "../public/images/send.png";
+
 const Header = () => {
     return (
         <div>
-            Header
+            <Link href="/">
+                <span>Instagram</span>
+            </Link>
+            <Image src={Add} alt="logo" width={30} height={30} />
+            <Image src={Heart} alt="logo" width={30} height={30} />
+            <Image src={Send} alt="logo" width={30} height={30} />
         </div>
     )
 }
