5bf2d7e557acbcfce1dcce9d085d1c5fb82a0f0f
aldrn <aldrn29@gmail.com>
2022-07-17 23:42:50 +0900

    feat : Footer 이미지 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Home from "../public/images/home.png";
+import Magnifier from "../public/images/magnifier.png";
+import Video from "../public/images/video.png";
+import Chat from "../public/images/chat.png";
+
 const Footer = () => {
     return (
         <div>
-            Footer
+            <Image src={Home} alt="logo" width={30} height={30} />
+            <Image src={Magnifier} alt="logo" width={30} height={30} />
+            <Image src={Video} alt="logo" width={30} height={30} />
+            <Image src={Chat} alt="logo" width={30} height={30} />
         </div>
     )
 }
