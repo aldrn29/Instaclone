147cf60be2e99fe12a7fead9fc3c1c4338b9f9c3
aldrn <aldrn29@gmail.com>
2022-09-20 23:19:19 +0900

    feat: 상단 스크롤 버튼 구현

--- components/Index.tsx
+++ components/Index.tsx
@@ -1,12 +1,28 @@
+import { useRef } from "react";
+import Image from "next/image";
 import styled from "styled-components";
 import Content from "./Content";
 import Card from "./Card";
+import Up from "../public/images/up.png";
 
 const Index = () => {
+    const ref = useRef<HTMLInputElement>();
     
+    const handleScroll = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
-        <Wrapper>
+        <Wrapper ref={ref}>
             <Card />
+            <Card />
+            <Card />
+            <ScrollButton onClick={handleScroll}>
+                <Image src={Up} alt="up" width={30} height={30} />
+            </ScrollButton>
         </Wrapper>
     )
 }
@@ -16,5 +32,13 @@ export default Index
 const Wrapper = styled.div`
     width: 100%;
     height: auto;
-    // background-color: skyblue;
+    position: relative;
+`;
+
+const ScrollButton = styled.div`
+    position: fixed;
+    right: 10px;
+    bottom: 60px;
+    background-color: white;
+    border-radius: 50%;
 `;
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -35,7 +35,7 @@ const Wrapper = styled.div`
     width: 100%;
     height: 50px;
     background-color: white;
-    position: absolute;
+    position: fixed;
     bottom: 0;
     display: flex;
     justify-content: space-around;
